25363e3b7ba45a28ef745f0b3697136a02945b01
Gouhar Mirza <mirza.gb21@gmail.com>
2025-04-17 08:07:30 +0530

    Change Theam

--- ScholarTracker/Controllers/HomeController.cs
+++ ScholarTracker/Controllers/HomeController.cs
@@ -12,7 +12,11 @@ namespace ScholarTracker.Controllers
         {
             return View();
         }
+        public ActionResult Dashbord1()
+        {
 
+            return View();
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
